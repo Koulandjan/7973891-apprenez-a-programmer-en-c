9864809dbc84d591a0ef72875e31773d31bd91be
Koulandjan <diabymacka3@gmail.com>
2025-11-05 10:36:56 -0500

    Instantiate Livre object and set properties
    Instantiate a Livre object and set its properties.

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,7 +1,9 @@
 ﻿// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
-
+livre monLivre = new Livre();
 // TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
-
+monLivre.Titre="Mansa";
+monLivre.Auteur="Koulandjan";
+monLivre.NombreDePages=330;
 // Afficher le contenu des champs de cet objet 
 Console.WriteLine("Le titre du livre est " + monLivre.Titre);
 Console.WriteLine("Son auteur est " + monLivre.Auteur);
